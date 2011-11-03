f223f83b6553871496f22c2bf25bc9ad864cb653
Alexandre Cossette <alex.cossette@gmail.com>
2011-11-03 10:18:57 -0400

    Initial Commit

---
+++ peg2html/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  peg2html
+//
+//  Created by Utilisateur on 11-11-03.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
